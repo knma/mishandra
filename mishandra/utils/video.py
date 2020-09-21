ff10eae32a617366cc6700fe7349f4c8f3916cab
knma <pavelzyr@gmail.com>
2020-09-21 03:16:36 +0300

    wip

--- mishandra/utils/video.py
+++ mishandra/utils/video.py
@@ -1,12 +1,12 @@
 from __future__ import print_function, absolute_import
 
-import sys, os
+import sys, os, glob
 import numpy as np
-import ffmpeg
+import ffmpeg, cv2
 import subprocess
 
 
-def make_video(frames, size=None, video_path=None, video_bitrate=8e6):
+def make_video(frames, frames_dir=None, size=None, video_path=None, video_bitrate=8e6):
   if video_path is None:
     video_dir = 'mishatemp'
     os.makedirs(video_dir, exist_ok=True)
@@ -15,16 +15,21 @@ def make_video(frames, size=None, video_path=None, video_bitrate=8e6):
   if os.path.isfile(video_path):
       os.remove(video_path)
 
-  if not len(frames):
-    print('Frames are missing')
-    return
+  if frames_dir is not None:
+    frame_paths = list(sorted(glob.glob(os.path.join(frames_dir, '*.*'))))
+    first_frame = cv2.imread(frame_paths[0])
+  else:
+    if not len(frames):
+      print('Frames are missing')
+      return
+    first_frame = frames[0]
 
-  if not isinstance(frames[0], np.ndarray):
+  if not isinstance(first_frame, np.ndarray):
     print('Wrong frames type')
     return
 
   if size is None:
-    size = frames[0].shape[1], frames[0].shape[0]
+    size = first_frame.shape[1], first_frame.shape[0]
 
   process = (
     ffmpeg
@@ -34,8 +39,12 @@ def make_video(frames, size=None, video_path=None, video_bitrate=8e6):
     .run_async(pipe_stdin=True)
   )
 
-  for frame in frames:
-    process.stdin.write(frame.astype(np.uint8).tobytes())
+  if frames_dir is not None:
+    for frame_path in frame_paths:
+      process.stdin.write(cv2.imread(frame_path).astype(np.uint8).tobytes())
+  else:
+    for frame in frames:
+      process.stdin.write(frame.astype(np.uint8).tobytes())
 
   process.stdin.close()
   process.wait()
